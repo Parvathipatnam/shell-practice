bfc517b4fd4006ecd65f6e97781aaa10724b7699
paravthi patnam <paravthi.patnam@gmail.com>
2026-01-30 13:12:29 +0530

    7th commit

---
+++ 05-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Please enter your username::";
+read USER_NAME
+
+echo "User name is $USER_NAME"
+echo "Please entr your password::"
+read -s PASSWORD
+
+echo "password is $PASSWORD"
